e0107c9a616a1406e2dacce4595217c810f7fb04
Endo Kazuki <endo.kazuki@skooler.jp>
2019-08-07 13:20:30 +0900

    Initial Commit

---
+++ MLSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLSample
+//
+//  Created by Endo Kazuki on 2019/08/07.
+//  Copyright © 2019 Skooler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
